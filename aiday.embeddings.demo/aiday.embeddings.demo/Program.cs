b4c38e3463ba5f6f823579b2019f8bd4738699f5
Marco Milani <emme.milani@hotmail.it>
2023-11-20 10:43:52 +0100

    "Disable nullability and enhance configuration services
    Nullability setting has been turned off in aiday.embeddings.demo.csproj for better compatibility. Additional package references for Dependency Injection and Configuration Binder have been included to streamline the configuration of services. Appsettings.json now contains a "Typesense" section to manage Typesense configuration easier. These modifications have been reflected in the `Program.cs`, which now sets up Typesense configuration from appsettings.json through DI and Configuration Binder."

--- aiday.embeddings.demo/aiday.embeddings.demo/Program.cs
+++ aiday.embeddings.demo/aiday.embeddings.demo/Program.cs
@@ -1,11 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using aiday.embeddings.typesense;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 var services = new ServiceCollection();
 ConfigureServices(services);
 
+var serviceProvider = services.BuildServiceProvider();
+var config = serviceProvider.GetService<IConfiguration>();
+var test = config!.GetValue<int>("Test");
+var tt = test;
 
 static void ConfigureServices(IServiceCollection services)
 {
@@ -15,5 +20,14 @@ static void ConfigureServices(IServiceCollection services)
         .Build();
             
     services.AddSingleton<IConfiguration>(configuration);
+
+    var section = configuration.GetSection("Typesense");
+    services.AddTypesense(config =>
+    {
+        config.Node.Port = section.GetValue<string>("Port");
+        config.Node.Protocol = section.GetValue<string>("Protocol");
+        config.ApiKey = section.GetValue<string>("ApiKey");
+        config.Node.Host = section.GetValue<string>("Host");
+    } );
 }
 
