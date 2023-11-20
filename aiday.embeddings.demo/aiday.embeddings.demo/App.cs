b5446ca5c2a716bd776f3cb73e7db2ed5b56f6c1
Marco Milani <emme.milani@hotmail.it>
2023-11-20 15:10:55 +0100

    Refactor code for managing embeddings
    The code has been refactored to make it more user-friendly and efficient. A menu has been added to give the user a choice between creating embeddings and querying. To simplify and improve the code structure, the code for creating and querying embeddings has been moved to separate methods within a new class (App.cs). This improves readability and code maintenance.

---
+++ aiday.embeddings.demo/aiday.embeddings.demo/App.cs
@@ -0,0 +1,71 @@
+using aiday.embeddings.demo.Models;
+using aiday.embeddings.demo.Services;
+using aiday.embeddings.typesense;
+using aiday.embeddings.typesense.Models;
+using Microsoft.Extensions.DependencyInjection;
+using mjm.nethelpers;
+
+namespace aiday.embeddings.demo;
+
+static class App
+{
+    private const string CollectionName = "aiday_demo";
+
+    public static async Task CreateEmbeddings(ServiceProvider serviceProvider)
+    {
+        var embeddingService = serviceProvider.GetService<IDemoEmbeddingService>();
+        var typeSenseClient = serviceProvider.GetService<ITypesenseClient>();
+        var csvService = serviceProvider.GetService<ICsvService>();
+
+        var faqs = csvService.ReadCsv("openai_faq.csv");
+
+        await RunnerHelper.RunAndManageException(() => typeSenseClient
+                .CreateCollection(new Schema(CollectionName, TypeSenseFeedback.GetSchema())),
+            exception =>
+            {
+                Console.WriteLine(exception.Message);
+                return Task.CompletedTask;
+            });
+
+        var totalTokenCost = 0;
+        foreach (var faq in faqs)
+        {
+            Console.WriteLine($"Creating embedding for: {faq.Question}");
+            var embedded = await embeddingService.CreateEmbeddings(faq.Question);
+            var typeSenseFeedback = new TypeSenseFeedback
+            {
+                Vectors = embedded.Embeddings,
+                Id = Guid.NewGuid().ToString("N"),
+                Date = DateTimeOffset.UtcNow.ToUnixTimeSeconds(),
+                Text = faq.Question,
+                Url = faq.Url
+            };
+            Console.WriteLine($"Created embedding for: {faq.Question} - Token: {embedded.Tokens}");
+            totalTokenCost += embedded.Tokens;
+            await typeSenseClient.CreateDocument("aiday_demo", typeSenseFeedback);
+            Console.WriteLine("Saved to typesense");
+        }
+
+        Console.WriteLine($"Total token cost: {totalTokenCost} ");
+    }
+
+    public static async Task QueryEmbeddings(ServiceProvider serviceProvider)
+    {
+        var typeSenseClient = serviceProvider.GetService<ITypesenseClient>();
+        var embeddingService = serviceProvider.GetService<IDemoEmbeddingService>();
+
+        Console.WriteLine("Enter query:");
+        var query = Console.ReadLine();
+        
+        Console.WriteLine("Creating embedding");
+        var embedded = await embeddingService.CreateEmbeddings(query);
+        
+        var searchResult = await typeSenseClient.VectorSearch<TypeSenseFeedback>( new MultiSearchVectorParameters(CollectionName, embedded.Embeddings));
+        
+        foreach (var searchResultHit in searchResult.Hits.OrderBy(o=>o.VectorDistance).Take(3))
+        {
+            Console.WriteLine($"Distance: {searchResultHit.VectorDistance} - {searchResultHit.Document.Text}");
+        }
+        
+    }
+}
--- aiday.embeddings.demo/aiday.embeddings.demo/Program.cs
+++ aiday.embeddings.demo/aiday.embeddings.demo/Program.cs
@@ -13,40 +13,15 @@ ServiceConfiguration.ConfigureServices(services);
 
 var serviceProvider = services.BuildServiceProvider();
 
-var embeddingService = serviceProvider.GetService<IDemoEmbeddingService>();
-var typeSenseClient = serviceProvider.GetService<ITypesenseClient>();
-var csvService = serviceProvider.GetService<ICsvService>();
+Console.WriteLine("Press c for create embeddings, any other key to query");
+var key = Console.ReadKey();
 
-var faqs = csvService.ReadCsv("openai_faq.csv");
+if (key.KeyChar == 'c')
+    await App.CreateEmbeddings(serviceProvider);
+else
+    await App.QueryEmbeddings(serviceProvider);
 
-await RunnerHelper.RunAndManageException( () => typeSenseClient
-    .CreateCollection(new Schema("aiday_demo", TypeSenseFeedback.GetSchema())), 
-    exception =>
-    {
-        Console.WriteLine(exception.Message);
-        return Task.CompletedTask;
-    });
 
-var totalTokenCost = 0;
-foreach (var faq in faqs)
-{
-    Console.WriteLine($"Creating embedding for: {faq.Question}");
-    var embedded = await embeddingService.CreateEmbeddings(faq.Question);
-    var typeSenseFeedback = new TypeSenseFeedback
-    {
-        Vectors = embedded.Embeddings,
-        Id = Guid.NewGuid().ToString("N"),
-        Date = DateTimeOffset.UtcNow.ToUnixTimeSeconds(),
-        Text = faq.Question,
-        Url = faq.Url
-    };
-    Console.WriteLine($"Created embedding for: {faq.Question} - Token: {embedded.Tokens}");
-    totalTokenCost+= embedded.Tokens;
-    await typeSenseClient.CreateDocument("aiday_demo", typeSenseFeedback);
-    Console.WriteLine("Saved to typesense");
-}
-
-Console.WriteLine($"Total token cost: {totalTokenCost} ");
 
 
 
