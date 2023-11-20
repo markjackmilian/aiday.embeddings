747fdd58f9e0d90a4d9c3af815486d6951790cf6
Marco Milani <emme.milani@hotmail.it>
2023-11-20 10:28:50 +0100

    Add configuration services and appsettings.json file
    Configuration services were added to the aiday.embeddings.demo project to support application-wide configuration options. This includes referencing Microsoft's extension packages for configuration, abstractions, and user secrets. An appsettings.json file was also added to house configuration options.

--- aiday.embeddings.demo/aiday.embeddings.demo/Program.cs
+++ aiday.embeddings.demo/aiday.embeddings.demo/Program.cs
@@ -1,6 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 var services = new ServiceCollection();
+ConfigureServices(services);
+
+
+static void ConfigureServices(IServiceCollection services)
+{
+    IConfiguration configuration = new ConfigurationBuilder()
+        .AddUserSecrets<Program>()
+        .AddJsonFile("appsettings.json", optional: false)
+        .Build();
+            
+    services.AddSingleton<IConfiguration>(configuration);
+}
 
